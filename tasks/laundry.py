c3e108ed31ce8054b3c7c288c9912aca7ee3663e
Xiaotian Le <lxtbell@gmail.com>
2016-10-29 23:50:37 -0500

    Add laundry stats

--- tasks/laundry.py
+++ tasks/laundry.py
@@ -5,6 +5,7 @@ import time
 import urllib2
 import json
 import redis
+import datetime
 from tasks.celery import app
 
 
@@ -33,3 +34,61 @@ def laundry_update():
         r.set(room_name, json.dumps(room_info))
 
     r.set("app.tasks.laundry.rooms", json.dumps(room_names))
+
+
+def laundry_stats_update():
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    room_names = json.loads(r.get("app.tasks.laundry.rooms"))
+
+    for room in room_names:
+        room_info = json.loads(r.get(room))
+        room_stats_json = r.get("app.tasks.laundry.stats.{}".format(room))
+
+        if room_stats_json:
+            room_stats = json.loads(room_stats_json)
+            room_stat = room_stats['room_stat']
+            machine_stats = room_stats['machine_stats']
+            last_timestamp = room_stats['timestamp']
+        else:
+            room_stat = {
+                'total_open_by_hour': [0.] * 24,
+                'total_usage_by_hour': [0.] * 24,
+                'average_usage_by_hour': [0.] * 24
+            }
+            machine_stats = {}
+            last_timestamp = 0.
+
+        current_timestamp = room_info['timestamp']
+        delta_t = min(5 * 60, current_timestamp - last_timestamp)
+
+        if delta_t > 0:
+            machine_in_use = 0
+
+            for machine in room_info['machine']:
+                machine_id = machine['label']
+                if machine_id not in machine_stats:
+                    machine_stats[machine_id] = {
+                        'total_powered': 0.,
+                        'total_usage': 0.
+                    }
+                machine_stat = machine_stats[machine_id]
+
+                machine_stat['total_powered'] += delta_t
+                if machine['status'] == 'In Use':
+                    machine_stat['total_usage'] += delta_t
+                    machine_in_use += 1
+                machine_stat['daily_usage'] = machine_stat['total_usage'] / max(86400, machine_stat['total_powered']) * 86400
+
+            last_date = datetime.datetime.fromtimestamp(last_timestamp)
+            current_date = datetime.datetime.fromtimestamp(current_timestamp)
+            hour = current_date.hour
+
+            room_stat['total_open_by_hour'][hour] += delta_t
+            room_stat['total_usage_by_hour'][hour] += delta_t * machine_in_use
+            room_stat['average_usage_by_hour'][hour] = room_stat['total_usage_by_hour'][hour] / room_stat['total_open_by_hour'][hour]
+
+        r.set("app.tasks.laundry.stats.{}".format(room), json.dumps({
+            'room_stat': room_stat,
+            'machine_stats': machine_stats,
+            'timestamp': current_timestamp
+        }))
--- test/tasks/laundry_test.py
+++ test/tasks/laundry_test.py
@@ -17,6 +17,32 @@ class TestLaundry(unittest.TestCase):
             self.assertIsInstance(room_data["networked"], basestring)
             self.assertIsInstance(room_data["timestamp"], float)
 
+    def test_laundry_stats(self):
+        laundry_update()
+        laundry_stats_update()
+
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        room_names = json.loads(r.get("app.tasks.laundry.rooms"))
+
+        for room in room_names:
+            room_stats = json.loads(r.get("app.tasks.laundry.stats.{}".format(room)))
+
+            room_stat = room_stats['room_stat']
+            machine_stats = room_stats['machine_stats']
+            self.assertIsInstance(room_stats["timestamp"], float)
+
+            for hour in range(24):
+                self.assertTrue(0 <= room_stat['total_open_by_hour'][hour])
+                self.assertTrue(0 <= room_stat['total_usage_by_hour'][hour])
+                self.assertTrue(0 <= room_stat['average_usage_by_hour'][hour] <= len(machine_stats))
+
+            for machine_id in machine_stats:
+                machine_stat = machine_stats[machine_id]
+
+                self.assertTrue(0 <= machine_stat['total_powered'])
+                self.assertTrue(0 <= machine_stat['total_usage'])
+                self.assertTrue(0 <= machine_stat['daily_usage'] <= 86400)
+
 
 if __name__ == '__main__':
     unittest.main()
